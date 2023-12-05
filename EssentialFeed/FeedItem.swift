8f19caac3427b9a13defbe02274f6bb9f3d43ebd
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2023-12-05 19:42:35 -0300

    Create FeedLoader and FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,8 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Marcos Amaral on 05/12/23.
+//
+
+import Foundation
---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Marcos Amaral on 05/12/23.
+//
+
+import Foundation
