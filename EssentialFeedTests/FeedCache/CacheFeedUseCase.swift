571aa1e3b8b16446b0ebf344ad937d57e4a0b36d
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2023-12-12 21:00:46 -0300

    Save command requests deletion of cached feed items

--- EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
@@ -7,29 +7,56 @@
 
 import XCTest
 
+import EssentialFeed
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount = deleteCachedFeedCallCount + 1
+    }
+}
+
 final class CacheFeedUseCase: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_shouldNotDeleteWhenCreatingCache() {
+        let store = FeedStore()
+        
+        let sut = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_shouldRequestsCacheDeletion() {
+        let store = FeedStore()
+        
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItems(), uniqueItems()]
+        
+        sut.save(items: items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: Helpers
+    func uniqueItems() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
-
 }
