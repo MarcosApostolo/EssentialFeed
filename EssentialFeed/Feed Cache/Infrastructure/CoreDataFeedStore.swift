0399ee05f54972e87e85469533c07f3eaa8374f6
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2023-12-20 12:07:42 -0300

    simplify CoreDataFeedStore code with new Result api

--- EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -24,44 +24,33 @@ public class CoreDataFeedStore: FeedStore {
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
-            do {
+            completion(Result(catching: {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
     }
     
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion(Result(catching: {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 
-                try context.save()
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+                return try context.save()
+            }))
         }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                if let cache = try ManagedCache.find(in: context) {
-                    completion(.success(CacheFeed(
+            completion(Result(catching: {
+                try ManagedCache.find(in: context).map { cache in
+                    return CacheFeed(
                         feed: cache.localFeed,
-                        timestamp: cache.timestamp)))
-                } else {
-                    completion(.success(.none))
+                        timestamp: cache.timestamp)
                 }
-            } catch {
-                completion(.failure(error))
-            }
+            }))
         }
     }
 }
