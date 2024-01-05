54647632b02fa3890c3ceacc48df92eabb909cd0
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-01-04 23:03:17 -0300

    Move view models into separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Marcos Amaral on 04/01/24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Marcos Amaral on 04/01/24.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
