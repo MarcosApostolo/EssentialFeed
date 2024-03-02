8944ac5e433ecbb3994cf18a8f55cfe611e56434
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-03-02 10:57:36 -0300

    Extract duplicated HTTPClientSpy

--- EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -14,7 +14,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     func test_should_init() {
         let (_, client) = makeSUT()
         
-        XCTAssertTrue(client.requestedURL.isEmpty)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_shouldRequestDataWithCorrectURL() {
@@ -26,7 +26,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             
         }
         
-        XCTAssertEqual(client.requestedURL, [url])
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_shouldReturnError() {
@@ -161,33 +161,5 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         action()
         
         wait(for: [exp], timeout: 1.0)
-    }
-    
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        
-        var requestedURL: [URL] {
-            return messages.map { $0.url }
-        }
- 
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            
-            return Task()
-         }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode statusCode: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURL[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)
-            
-            messages[index].completion(.success((data, response!)))
-        }
     }
 }
