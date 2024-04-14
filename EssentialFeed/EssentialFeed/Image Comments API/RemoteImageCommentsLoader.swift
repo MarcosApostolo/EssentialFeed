796a1514ab73d015a5a142db093706fed23433ab
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-14 17:31:54 -0300

    Replace RemoteImageCommentsLoader with generic RemoteLoader to remote duplication

--- EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -7,44 +7,10 @@
 
 import Foundation
 
-public class RemoteImageCommentsLoader {
-    private let client: HTTPClient
-    private let url: URL
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-    
-    public init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else {
-                return
-            }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteImageCommentsLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try ImageCommentsMapper.map(data, from: response)
-            
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentsLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(client: client, url: url, mapper: ImageCommentsMapper.map)
     }
 }
--- EssentialFeed/EssentialFeedTests/Image Comments API/LoadCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/LoadCommentsFromRemoteUseCaseTests.swift
@@ -9,44 +9,6 @@ import XCTest
 import EssentialFeed
 
 final class LoadCommentsFromRemoteUseCaseTests: XCTestCase {
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://some-url")!
-        
-        let (sut, client) = makeSUT(url: url)
-                
-        sut.load() { _ in
-            
-        }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWithResult: .failure(RemoteImageCommentsLoader.Error.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            
-            client.complete(with: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon2xxHTTPResponse() {
         let (sut, client) = makeSUT()
         
@@ -115,29 +77,12 @@ final class LoadCommentsFromRemoteUseCaseTests: XCTestCase {
             }
         }
     }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "https://some-url")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentsLoader? = RemoteImageCommentsLoader(client: client, url: url)
-        
-        var capturedResult = [RemoteImageCommentsLoader.Result]()
-                
-        sut?.load(completion: {
-            capturedResult.append($0)
-        })
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJson(with: []))
-        
-        XCTAssertTrue(capturedResult.isEmpty)
-    }
-    
+
     // MARK: Helpers
     private func makeSUT(url: URL = URL(string: "https://some-url")!, file: StaticString = #filePath, line: UInt = #line) -> (RemoteImageCommentsLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         
-        let sut = RemoteImageCommentsLoader(client: client, url: url)
+        let sut = RemoteImageCommentsLoader(url: url, client: client)
         
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
