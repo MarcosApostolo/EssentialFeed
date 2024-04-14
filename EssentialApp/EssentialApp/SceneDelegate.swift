a1cc1280ee37277dbe1e8d056f1aa24646881050
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-14 17:36:39 -0300

    Replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -32,7 +32,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private lazy var remoteFeedLoader: RemoteFeedLoader = {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
-        return RemoteFeedLoader(client: httpClient, url: remoteURL)
+        return RemoteFeedLoader(url: remoteURL, client: httpClient)
     }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -110,3 +110,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 }
+
+extension RemoteFeedLoader: FeedLoader where Resource == [FeedImage] {}
--- EssentialFeed/EssentialFeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
@@ -9,45 +9,6 @@ import XCTest
 import EssentialFeed
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://some-url")!
-        
-        let (sut, client) = makeSUT(url: url)
-                
-        sut.load() { _ in
-            
-        }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            
-            client.complete(with: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
@@ -96,28 +57,11 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "https://some-url")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(client: client, url: url)
-        
-        var capturedResult = [RemoteFeedLoader.Result]()
-                
-        sut?.load(completion: {
-            capturedResult.append($0)
-        })
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJson(with: []))
-        
-        XCTAssertTrue(capturedResult.isEmpty)
-    }
-    
     // MARK: Helpers
     private func makeSUT(url: URL = URL(string: "https://some-url")!, file: StaticString = #filePath, line: UInt = #line) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         
-        let sut = RemoteFeedLoader(client: client, url: url)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
