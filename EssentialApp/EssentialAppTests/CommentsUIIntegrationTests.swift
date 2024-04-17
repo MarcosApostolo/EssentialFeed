140e4187607dd1c71e9f31c9d5dab23b1937db56
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-17 18:35:09 -0300

    Dispatches from background to main queue

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -97,16 +97,16 @@ class CommentsUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [comment0])
     }
     
-    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
-//        let (sut, loader) = makeSUT()
-//        sut.loadViewIfNeeded()
-//
-//        let exp = expectation(description: "Wait for background queue")
-//        DispatchQueue.global().async {
-//            loader.completeFeedLoading(at: 0)
-//            exp.fulfill()
-//        }
-//        wait(for: [exp], timeout: 1.0)
+    func test_loadCommentsCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeCommentsLoading(at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
 
     func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
