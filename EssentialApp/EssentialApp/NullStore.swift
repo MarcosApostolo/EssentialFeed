6391dbaad40f3f9eea70c4dca875c4ba0ab22cd5
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-04-18 17:56:20 -0300

    Make in memory FeedImageDataStore implementation sync

--- EssentialApp/EssentialApp/NullStore.swift
+++ EssentialApp/EssentialApp/NullStore.swift
@@ -8,7 +8,19 @@
 import Foundation
 import EssentialFeed
 
-class NullStore: FeedImageDataStore {
+class NullStore: FeedImageDataStore & FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+
     func insert(_ data: Data, for url: URL) throws {}
 
     func retrieve(dataForURL url: URL) throws -> Data? { .none }
